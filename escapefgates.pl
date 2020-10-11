468ead6473267dbadf96478eaf785e1927bfae9f
Pedro Cella <pedrovscella@gmail.com>
2020-10-11 11:39:45 -0300

    Adicionando a disciplina de calculo

--- escapefgates.pl
+++ escapefgates.pl
@@ -1,3 +1,5 @@
+%Comandos
+
 iniciar:-write('Parabéns por passar no vestibular e ingressar nessa nova fase da vida, a FACULDADE, será que você tem o que é preciso para sobreviver.'),nl,
          write('Tente não jubilar e consiga se formar em 5 anos. Bem vindo ao curso de Engenharia.Bem vindo ao FGAtes.'),nl,comandos.
 
@@ -9,4 +11,23 @@ comandos:-nl,write('Antes de começar você deve entender algumas instruções s
 trancar:-write('É uma pena que não tenha conseguido se formar. Mais sorte da próxima vez, até logo'),nl,
          write('ESCAPE FGATES: Você não se formou!'),!.
 
-jogar:-nl,write('Teste').
+jogar:-nl,write('Bem-vindo, parabéns por passar o vestibular, vocễ agora deve enfrentar os 5 anos de faculdade, será que você irá sobreviver '),nl,
+       write('Como calouro você não escolhe, por isso deverá enfrentar seus primeiros 2 semestres passando nas matérias que lhe foram dadas. Boa sorte!'),nl,calculo.
+
+%Matérias
+
+calculo:-nl,write('Seu primeiro desafio, Cálculo, a matéria que é falada por todos os estudantes de engenharia como sua primeira dificuldade'),nl,
+         write('essa matéria pode ser tanto fácil como somar 1 com 2, como tamem pode ser impossíve de se passar, como você irá se sair nessa?'),nl,
+         nl,write('Resolva:'),nl,
+         write('1 + 2 = ?'),nl,
+         write('Resposta: '),
+         read(Resposta),
+         (Resposta=:=3 -> write('Parabéns por passar em seu primeiro semestre, veremos até quando irá durar.'),nl,intro_engenharia ; write('É uma pena, mas você reprovou a matéria, por causa disso terá que fazê-la novamente'),nl,calculo).
+
+intro_engenharia:-write('Teste').
+
+%eletricidade:-.ligue as luzes da sala
+
+%fso:-.
+
+%economia_energia:-.
