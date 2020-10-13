61d2f11905e6218eb9da1e39c04370213c641039
bielrossi15 <bielrossiborba@gmail.com>
2020-10-13 11:42:17 -0300

    Criando desafio para materia de S Auto

--- escapefgates.pl
+++ escapefgates.pl
@@ -10,3 +10,70 @@ trancar:-write('É uma pena que não tenha conseguido se formar. Mais sorte da p
          write('ESCAPE FGATES: Você não se formou!'),!.
 
 jogar:-nl,write('Teste').
+
+
+sa:-     write("Você esta cursando a materia de Sistemas Automotivos e 
+            precisa ajudar seu amigo a comprar um carro seminovo, 
+            ponha seus conhecimentos em pratica, inspecione os carros
+             e escolha aquele que nao tem nenhum problema!!!"
+            ),nl,carro1.
+carro1:- nl,write("Carro 1: "),nl,
+         write("1 - Olhar freio"),nl,
+         write("2 - Olhar motor"),nl,
+         write("3 - Olhar rodas"),nl,
+         write("4 - Olhar painel"),nl,
+         write("5 - Olhar bateria"),nl,
+         read(Escolha1),nl,
+         (Escolha1 \== 1 -> write('Nada aqui!'),nl,carro1 ; write('Voce notou um problema no freio! Pera, o carro nao possui o pedal??')),nl,carro2.
+carro2:- nl,write("Carro 2: "),nl,
+         write("1 - Olhar freio"),nl,
+         write("2 - Olhar motor"),nl,
+         write("3 - Olhar rodas"),nl,
+         write("4 - Olhar painel"),nl,
+         write("5 - Olhar bateria"),nl,
+         read(Escolha2),nl,
+         (Escolha2 \== 3 -> write('Nada aqui!'),nl,carro2 ; write('Voce notou que o carro nao possui rodas...')),nl,carro3.
+carro3:- nl,write("Carro 3: "),nl,
+         write("1 - Olhar freio"),nl,
+         write("2 - Olhar motor"),nl,
+         write("3 - Olhar rodas"),nl,
+         write("4 - Olhar painel"),nl,
+         write("5 - Olhar bateria"),nl,
+         read(Escolha3),nl,
+         (Escolha3 \== 5 -> write('Nada aqui!'),nl,carro3 ; write('Voce notou que a bateria do carro foi trocada por um tijolo...')),nl,carro4.
+carro4:- nl,write("Carro 4: "),nl,
+         write("1 - Olhar freio"),nl,
+         write("2 - Olhar motor"),nl,
+         write("3 - Olhar rodas"),nl,
+         write("4 - Olhar painel"),nl,
+         write("5 - Olhar bateria"),nl,
+         read(Escolha4),nl,
+         (Escolha4 \== 3 -> write('Nada aqui!'),nl,carro4 ; write('Voce notou que as rodas do carro estao estranhamente quadradas...')),nl,carro5.
+carro5:- nl,write("Carro 5: "),nl,
+         write("1 - Olhar freio"),nl,
+         write("2 - Olhar motor"),nl,
+         write("3 - Olhar rodas"),nl,
+         write("4 - Olhar painel"),nl,
+         write("5 - Olhar bateria"),nl,
+         read(Escolha5),nl,
+         (Escolha5 \== 5 -> write('Nada aqui!'),nl,carro5 ; write('Voce cansou de procurar algo nesse carro..')),nl,carro6.  
+carro6:- nl,write("Carro 6: "),nl,
+         write("1 - Olhar freio"),nl,
+         write("2 - Olhar motor"),nl,
+         write("3 - Olhar rodas"),nl,
+         write("4 - Olhar painel"),nl,
+         write("5 - Olhar bateria"),nl,
+         read(Escolha6),nl,
+         (Escolha6 \== 2 -> write('Nada aqui!'),nl,carro6 ; write('Voce notou que esse carro... Nao possui motor???')),nl,escolha.    
+escolha:- nl,write('Agora que viu todas as opcoes, escolha o carro!!'),nl,
+          write('1 - Carro 1'),nl,
+          write('2 - Carro 2'),nl,
+          write('3 - Carro 3'),nl,
+          write('4 - Carro 4'),nl,
+          write('5 - Carro 5'),nl,
+          write('6 - Carro 6'),nl,
+          read(EscolhaFinal),nl,
+          (EscolhaFinal \== 5 -> write('Seu amigo comprou o carro com defeito, o professor ficou sabendo e voce foi reprovado!'),sa,nl; write('Parabens!! Agora voce tem carona e a aprovacao naa disciplina!!')).
+
+
+
