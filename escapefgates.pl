0b7895f0da850a5401a89cce16f3ff01fc467823
bielrossi15 <bielrossiborba@gmail.com>
2020-10-14 14:25:57 -0300

    Desafio SA

--- escapefgates.pl
+++ escapefgates.pl
@@ -12,17 +12,15 @@ trancar:-write('É uma pena que não tenha conseguido se formar. Mais sorte da p
 jogar:-nl,write('Teste').
 
 
-sauto:-     write("Você esta cursando a materia de Sistemas Automotivos e 
-            precisa ajudar seu amigo a comprar um carro seminovo, 
-            ponha seus conhecimentos em pratica, inspecione os carros
-             e escolha aquele que nao tem nenhum problema!!!"
+sauto:-     write("Você esta cursando a materia de Sistemas Automotivos e precisa ajudar seu amigo a comprar um carro seminovo, 
+                   ponha seus conhecimentos em pratica, inspecione os carros e escolha aquele que nao tem nenhum problema!!!"
             ),nl,carro1.
 carro1:- nl,write("Carro 1: "),nl,
          write("1 - Olhar freio"),nl,
          write("2 - Olhar motor"),nl,
          write("3 - Olhar rodas"),nl,
          write("4 - Olhar painel"),nl,
-         write("5 - Olhar bateria"),nl,
+         write("5 - Olhar Vida"),nl,
          read(Escolha1),nl,
          (Escolha1 \== 1 -> write('Nada aqui!'),nl,carro1 ; write('Voce notou um problema no freio! Pera, o carro nao possui o pedal??')),nl,carro2.
 carro2:- nl,write("Carro 2: "),nl,
@@ -30,7 +28,7 @@ carro2:- nl,write("Carro 2: "),nl,
          write("2 - Olhar motor"),nl,
          write("3 - Olhar rodas"),nl,
          write("4 - Olhar painel"),nl,
-         write("5 - Olhar bateria"),nl,
+         write("5 - Olhar Vida"),nl,
          read(Escolha2),nl,
          (Escolha2 \== 3 -> write('Nada aqui!'),nl,carro2 ; write('Voce notou que o carro nao possui rodas...')),nl,carro3.
 carro3:- nl,write("Carro 3: "),nl,
@@ -38,15 +36,15 @@ carro3:- nl,write("Carro 3: "),nl,
          write("2 - Olhar motor"),nl,
          write("3 - Olhar rodas"),nl,
          write("4 - Olhar painel"),nl,
-         write("5 - Olhar bateria"),nl,
+         write("5 - Olhar Vida"),nl,
          read(Escolha3),nl,
-         (Escolha3 \== 5 -> write('Nada aqui!'),nl,carro3 ; write('Voce notou que a bateria do carro foi trocada por um tijolo...')),nl,carro4.
+         (Escolha3 \== 5 -> write('Nada aqui!'),nl,carro3 ; write('Voce notou que a Vida do carro foi trocada por um tijolo...')),nl,carro4.
 carro4:- nl,write("Carro 4: "),nl,
          write("1 - Olhar freio"),nl,
          write("2 - Olhar motor"),nl,
          write("3 - Olhar rodas"),nl,
          write("4 - Olhar painel"),nl,
-         write("5 - Olhar bateria"),nl,
+         write("5 - Olhar Vida"),nl,
          read(Escolha4),nl,
          (Escolha4 \== 3 -> write('Nada aqui!'),nl,carro4 ; write('Voce notou que as rodas do carro estao estranhamente quadradas...')),nl,carro5.
 carro5:- nl,write("Carro 5: "),nl,
@@ -54,7 +52,7 @@ carro5:- nl,write("Carro 5: "),nl,
          write("2 - Olhar motor"),nl,
          write("3 - Olhar rodas"),nl,
          write("4 - Olhar painel"),nl,
-         write("5 - Olhar bateria"),nl,
+         write("5 - Olhar Vida"),nl,
          read(Escolha5),nl,
          (Escolha5 \== 5 -> write('Nada aqui!'),nl,carro5 ; write('Voce cansou de procurar algo nesse carro..')),nl,carro6.  
 carro6:- nl,write("Carro 6: "),nl,
@@ -62,7 +60,7 @@ carro6:- nl,write("Carro 6: "),nl,
          write("2 - Olhar motor"),nl,
          write("3 - Olhar rodas"),nl,
          write("4 - Olhar painel"),nl,
-         write("5 - Olhar bateria"),nl,
+         write("5 - Olhar Vida"),nl,
          read(Escolha6),nl,
          (Escolha6 \== 2 -> write('Nada aqui!'),nl,carro6 ; write('Voce notou que esse carro... Nao possui motor???')),nl,escolha.    
 escolha:- nl,write('Agora que viu todas as opcoes, escolha o carro!!'),nl,
@@ -88,4 +86,20 @@ senha:-     write("Senha criptografada = cbodp"),nl,
             ; Senha == 'dica' -> write("Dica final: a+1, b+1, c+1,..."),nl,senha
             ).
 
+
+sa:- nl,write("Você esta cursando a materia de Sistemas Aeroespaciais, fez um aviaozinho de papel para a materia e agora precisa jogar"),nl,
+    write("Jogue o aviaozinho na oportunidade correta e evite que ele quebre!!!"),nl,
+    nl,write("Oportunidade 1 - Sem vento e chovendo"),nl,
+    write("Oportunidade 2 - Muito vento e ensolarado"),nl,
+    write("Oportunidade 3 - Muito vento e chovendo"),nl,
+    write("Oportunidade 4 - Sem vento e ensolarado"),nl,
+    write("Oportunidade 5- Pouco vento e chovendo"),nl,
+    read(Escolha),nl,
+    (
+        Escolha == 1 -> write('Choveu no seu aviaozinho e ele ficou encharcado... Reprovado.'),sa,nl; 
+        Escolha == 2 -> write('O vento levou seu aviaozinho ate a ponte alta... Reprovado.'),sa,nl; 
+        Escolha == 3 -> write('Voce foi inteligente e testou dentro do auditorio, parabens! Aprovado!'),nl; 
+        Escolha == 4 -> write('O clima estava perfeito, mas sua mira nao... O aviaozinho bateu na parede e desmontou... Reprovado.'),sa,nl; 
+        Escolha == 5 -> write('Tinha tudo pra dar errado... E deu! Reprovado.'),sa,nl
+    ).
 
