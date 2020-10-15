c4201eb0a7ecfa717df8bc0594756bb5124d1811
Pedro Cella <pedrovscella@gmail.com>
2020-10-15 14:12:07 -0300

    Adicionando o contador nas matérias da branch

--- escapefgates.pl
+++ escapefgates.pl
@@ -29,12 +29,11 @@ comandos:-nl,write('Antes de começar você deve entender algumas instruções s
 trancar:-write('É uma pena que não tenha conseguido se formar. Mais sorte da próxima vez, até logo'),nl,
          write('ESCAPE FGATES: Você não se formou!'),!.
 
-jogar:-nl,write('Teste').
-
 jogar:-nl,write('Bem-vindo, parabéns por passar o vestibular, vocễ agora deve enfrentar os 5 anos de faculdade, será que você irá sobreviver '),nl,
        write('Como calouro você não escolhe, por isso deverá enfrentar seus primeiros 2 semestres passando nas matérias que lhe foram dadas. Boa sorte!'),nl,calculo.
 
 jubilar:-write('Você reprovou mais de 3 vezes, você foi Jubilado!'),nl,!.
+
 
 formou_aeroespacial:-write('Parabéns você se formou em Aeroespacial e ganhou o jogo!'),nl,
              write('ESCAPE FGATES: Você se formou!').
@@ -156,81 +155,6 @@ eletromag:-write('Parabéns, por causa da escolha do seu curso, você veio para 
            ; Tomada =:= 6 -> write('Parabéns você escolheu uma das tomadas certas! Aprovado!'),nl,zerar_contador
            ; write('Você escolheu uma tomada que não funcionava, você e seu grupo reprovaram!'),nl,mainIncrementa,eletromag).
 
-%Ano3
-
-eletricidade:-write('Bem-vindo(a) a eletricidade!'),nl,
-              write('Nessa matéria você terá que ligar todos as luzes da sala, em apenas 4 movimentos, você sabe que:'),nl,
-              write('As luzes da sala estão desligadas'),nl,
-              write('A sala possui 4 interruptores correspondendo as 5 luzes na sala, cada uma atua de uma maneira diferente;'),nl,
-              write('interruptor 1 - liga a luz 1'),nl,
-              write('interruptor 2 - liga as luzes 1, 2 e 5'),nl,
-              write('interruptor 3 - liga as luzes 3 e 4, e desliga a luz 4 caso ela esteja ligada'),nl,
-              write('interruptor 4 - liga a luz 4'),nl,
-              write('Suas respostas: '),nl,
-              nl,write('Escolha 1: '),read(Escolha1),nl,
-              (Escolha1 \== 4 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 1 correta!')),nl,
-              write('Escolha 2: '),read(Escolha2),nl,
-              (Escolha2 \== 2 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 2 correta!')),nl,
-              write('Escolha 3: '),read(Escolha3),nl,
-              (Escolha3 \== 3 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 3 correta!')),nl,
-              write('Escolha 4: '),read(Escolha4),nl,
-              (Escolha4 \== 4 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 4 correta!')),nl,
-              write('Parabéns você foi aprovado!'),nl,zerar_contador.
-
-
-fisica_moderna :- write('Você chegou a física moderna, dentre as físicas é a mais atual, e por isso a mais complexa'),nl,
-                  write('Para passar basta responder a pergunta, o gato de Schrödinger está: '),nl,
-                  write('1- Vivo;'),nl,
-                  write('2- Morto;'),nl,
-                  write('Sua resposta:  '),nl,
-                  read(Resposta),
-                  random(0, 100, Resultado_vivo),write(Resultado_vivo),nl,
-                  random(0, 100, Resultado_morto),write(Resultado_morto),nl,
-                  (Resposta =:= 1, Resultado_vivo >= Resultado_morto -> write('Certo o gato tem mais chance de estar vivo!'),nl,write('Aprovado!'),zerar_contador 
-                  ; Resposta =:= 2, Resultado_morto >= Resultado_vivo -> write('Certo o gato tem mais chance de estar morto!'),nl,write('Aprovado!'),zerar_contador
-                  ; write('Reprovado! Terá que fazer a matéria novamente!'),nl,mainIncrementa, fisica_moderna).
-
-
-fso:- write('Você chegou em FSO - Fundamentos de Sistemas Operacionais'),nl,
-      write('Para você passar é simples digite o nome do melhor sistema operacional'),nl,
-      write('Não se esqueça de usar as aspas simples para escrever'),nl,
-      write('Sua resposta: '),read(Resposta),nl,
-      (Resposta = 'Linux' -> write('Aprovado!'),nl,zerar_contador ; write('Reprovado!'),nl,mainIncrement,fso).
-
-%Ano4
-
-subtractor_bateria(Bateria, Celular) :- Bateria - 20 #= Celular, write(Celular).
-counter_bateria(Bateria, Celular) :- Bateria + 0 #= Celular,write(Celular).
-
-
-economia_energia:- write('Bem-vindo(a) a matéria de Economia de Energia, para passar basta não deixar seu celular descarregar.'),nl,
-                   write('Para isso você terá que escolher que aplicativos você irá fechar, cada aplicativo consome uma porcentagem de energia'),nl,
-                   write('Faça a escolha certa e evite que seu celular descarregue!'),nl,
-                   write('1º round:'),nl,
-                   write('1- Youtube'),nl,
-                   write('2- Spotify'),nl,
-                   read(Aplicativo1),(Aplicativo1 =:= 2 -> write('Bateria: '),Bateria1 = 100,subtractor_bateria(Bateria1, Celular1),write('%') ; write('Bateria: '),Bateria1 = 100,counter_bateria(Bateria1, Celular1),write('%')),nl,
-                   write('2º round:'),nl,
-                   write('1- Instagram'),nl,
-                   write('2- Snapchat'),nl,
-                   read(Aplicativo2),(Aplicativo2 =:= 2 -> write('Bateria: '),subtractor_bateria(Celular1, Celular2),write('%') ; write('Bateria: '),counter_bateria(Celular1, Celular2),write('%')),nl,
-                   write('3º round:'),nl,
-                   write('1- Twitter'),nl,
-                   write('2- Whatsapp'),nl,
-                   read(Aplicativo3),(Aplicativo3 =:= 1 -> write('Bateria: '),subtractor_bateria(Celular2, Celular3),write('%') ; write('Bateria: '),counter_bateria(Celular2, Celular3),write('%')),nl,
-                   write('4º round:'),nl,
-                   write('1- Facebook'),nl,
-                   write('2- Reddit'),nl,
-                   read(Aplicativo4),(Aplicativo4 =:= 2 -> write('Bateria: '),subtractor_bateria(Celular3, Celular4),write('%') ; write('Bateria: '),counter_bateria(Celular3, Celular4),write('%')),nl,
-                   write('5º round:'),nl,
-                   write('1- Netflix'),nl,
-                   write('2- Google Chrome'),nl,
-                   read(Aplicativo5),(Aplicativo5 =:= 1 -> write('Bateria: '),subtractor_bateria(Celular4, Celular5),write('%'),nl ; write('Bateria: '),counter_bateria(Celular4, Celular5),write('%')),nl,
-                   (Celular5 =< 0 -> write('Reprovado! Terá que fazer a matéria novamente!'),nl,mainIncrementa,economia_energia ; write('Aprovado!'),zerar_contador).
-
-
-% nao to lembrado do ano 
-
 sauto:-     write('Você esta cursando a materia de Sistemas Automotivos e precisa ajudar seu amigo a comprar um carro seminovo, 
                    ponha seus conhecimentos em pratica, inspecione os carros e escolha aquele que nao tem nenhum problema!!!'
             ),nl,carro1.
@@ -290,19 +214,7 @@ escolha:- nl,write('Agora que viu todas as opcoes, escolha o carro!!'),nl,
           write('5 - Carro 5'),nl,
           write('6 - Carro 6'),nl,
           read(EscolhaFinal),nl,
-          (EscolhaFinal \== 5 -> write('Seu amigo comprou o carro com defeito, o professor ficou sabendo e voce foi reprovado!'),sauto,nl; write('Parabens!! Agora voce tem carona e a aprovacao naa disciplina!!')).
-
-bd:- nl,write('Você esta cursando a materia de Bancos de dados e percebeu que salvou o nome de um usuario sem a senha!!'),nl,
-     write('Tente descobrir a senha do usuario para poder entregar o trabalho final!!'),nl,senha.
-senha:-     write('Senha criptografada = cbodp'),nl,
-            write('Dica: A senha esta muito mais perto do que voce imagina!!'),nl,
-            write('Se precisar de dicas sobre a cifra, escreva 'cola' ou 'dica' na resposta'),nl,
-            read(Senha),nl,
-            (Senha \== 'banco', Senha \== 'cola', Senha \== 'dica' -> write('Senha errada, faca a materia de novo..'),nl,bd
-            ; Senha == 'banco' -> write('Aprovado!'),nl
-            ; Senha == 'cola' -> write('O professor pegou voce colando... Reprovado!!!'),nl,bd
-            ; Senha == 'dica' -> write('Dica final: a+1, b+1, c+1,...'),nl,senha
-            ).
+          (EscolhaFinal \== 5 -> write('Seu amigo comprou o carro com defeito, o professor ficou sabendo e voce foi reprovado!'),nl,mainIncrementa,sauto; write('Parabens!! Agora voce tem carona e a aprovacao naa disciplina!!')),nl,zerar_contador.
 
 sa:- nl,write('Você esta cursando a materia de Sistemas Aeroespaciais, fez um aviaozinho de papel para a materia e agora precisa jogar'),nl,
     write('Jogue o aviaozinho na oportunidade correta e evite que ele quebre!!!'),nl,
@@ -313,12 +225,76 @@ sa:- nl,write('Você esta cursando a materia de Sistemas Aeroespaciais, fez um a
     write('Oportunidade 5- Pouco vento e chovendo'),nl,
     read(Escolha),nl,
     (
-        Escolha == 1 -> write('Choveu no seu aviaozinho e ele ficou encharcado... Reprovado.'),sa,nl; 
-        Escolha == 2 -> write('O vento levou seu aviaozinho ate a ponte alta... Reprovado.'),sa,nl; 
-        Escolha == 3 -> write('Voce foi inteligente e testou dentro do auditorio, parabens! Aprovado!'),nl; 
-        Escolha == 4 -> write('O clima estava perfeito, mas sua mira nao... O aviaozinho bateu na parede e desmontou... Reprovado.'),sa,nl; 
-        Escolha == 5 -> write('Tinha tudo pra dar errado... E deu! Reprovado.'),sa,nl
+        Escolha == 1 -> write('Choveu no seu aviaozinho e ele ficou encharcado... Reprovado.'),mainIncrementa,sa,nl; 
+        Escolha == 2 -> write('O vento levou seu aviaozinho ate a ponte alta... Reprovado.'),mainIncrementa,sa,nl; 
+        Escolha == 3 -> write('Voce foi inteligente e testou dentro do auditorio, parabens! Aprovado!'),nl,zerar_contador; 
+        Escolha == 4 -> write('O clima estava perfeito, mas sua mira nao... O aviaozinho bateu na parede e desmontou... Reprovado.'),mainIncrementa,sa,nl; 
+        Escolha == 5 -> write('Tinha tudo pra dar errado... E deu! Reprovado.'),mainIncrementa,sa,nl
     ).
+
+%Ano3
+
+fisica_moderna :- write('Você chegou a física moderna, dentre as físicas é a mais atual, e por isso a mais complexa'),nl,
+                  write('Para passar basta responder a pergunta, o gato de Schrödinger está: '),nl,
+                  write('1- Vivo;'),nl,
+                  write('2- Morto;'),nl,
+                  write('Sua resposta:  '),nl,
+                  read(Resposta),
+                  random(0, 100, Resultado_vivo),write(Resultado_vivo),nl,
+                  random(0, 100, Resultado_morto),write(Resultado_morto),nl,
+                  (Resposta =:= 1, Resultado_vivo >= Resultado_morto -> write('Certo o gato tem mais chance de estar vivo!'),nl,write('Aprovado!'),zerar_contador 
+                  ; Resposta =:= 2, Resultado_morto >= Resultado_vivo -> write('Certo o gato tem mais chance de estar morto!'),nl,write('Aprovado!'),zerar_contador
+                  ; write('Reprovado! Terá que fazer a matéria novamente!'),nl,mainIncrementa, fisica_moderna).
+
+
+fso:- write('Você chegou em FSO - Fundamentos de Sistemas Operacionais'),nl,
+      write('Para você passar é simples digite o nome do melhor sistema operacional'),nl,
+      write('Não se esqueça de usar as aspas simples para escrever'),nl,
+      write('Sua resposta: '),read(Resposta),nl,
+      (Resposta = 'Linux' -> write('Aprovado!'),nl,zerar_contador ; write('Reprovado!'),nl,mainIncrementa,fso).
+
+%Ano4
+
+subtractor_bateria(Bateria, Celular) :- Bateria - 20 #= Celular, write(Celular).
+counter_bateria(Bateria, Celular) :- Bateria + 0 #= Celular,write(Celular).
+
+economia_energia:- write('Bem-vindo(a) a matéria de Economia de Energia, para passar basta não deixar seu celular descarregar.'),nl,
+                   write('Para isso você terá que escolher que aplicativos você irá fechar, cada aplicativo consome uma porcentagem de energia'),nl,
+                   write('Faça a escolha certa e evite que seu celular descarregue!'),nl,
+                   write('1º round:'),nl,
+                   write('1- Youtube'),nl,
+                   write('2- Spotify'),nl,
+                   read(Aplicativo1),(Aplicativo1 =:= 2 -> write('Bateria: '),Bateria1 = 100,subtractor_bateria(Bateria1, Celular1),write('%') ; write('Bateria: '),Bateria1 = 100,counter_bateria(Bateria1, Celular1),write('%')),nl,
+                   write('2º round:'),nl,
+                   write('1- Instagram'),nl,
+                   write('2- Snapchat'),nl,
+                   read(Aplicativo2),(Aplicativo2 =:= 2 -> write('Bateria: '),subtractor_bateria(Celular1, Celular2),write('%') ; write('Bateria: '),counter_bateria(Celular1, Celular2),write('%')),nl,
+                   write('3º round:'),nl,
+                   write('1- Twitter'),nl,
+                   write('2- Whatsapp'),nl,
+                   read(Aplicativo3),(Aplicativo3 =:= 1 -> write('Bateria: '),subtractor_bateria(Celular2, Celular3),write('%') ; write('Bateria: '),counter_bateria(Celular2, Celular3),write('%')),nl,
+                   write('4º round:'),nl,
+                   write('1- Facebook'),nl,
+                   write('2- Reddit'),nl,
+                   read(Aplicativo4),(Aplicativo4 =:= 2 -> write('Bateria: '),subtractor_bateria(Celular3, Celular4),write('%') ; write('Bateria: '),counter_bateria(Celular3, Celular4),write('%')),nl,
+                   write('5º round:'),nl,
+                   write('1- Netflix'),nl,
+                   write('2- Google Chrome'),nl,
+                   read(Aplicativo5),(Aplicativo5 =:= 1 -> write('Bateria: '),subtractor_bateria(Celular4, Celular5),write('%'),nl ; write('Bateria: '),counter_bateria(Celular4, Celular5),write('%')),nl,
+                   (Celular5 =< 0 -> write('Reprovado! Terá que fazer a matéria novamente!'),nl,mainIncrementa,economia_energia ; write('Aprovado!'),zerar_contador).
+
+
+bd:- nl,write('Você esta cursando a materia de Bancos de dados e percebeu que salvou o nome de um usuario sem a senha!!'),nl,
+     write('Tente descobrir a senha do usuario para poder entregar o trabalho final!!'),nl,senha.
+senha:-     write('Senha criptografada = cbodp'),nl,
+            write('Dica: A senha esta muito mais perto do que voce imagina!!'),nl,
+            write('Se precisar de dicas sobre a cifra, escreva '),write(' cola '),write(' ou '),write(' dica '),write(' na resposta'),nl,
+            read(Senha),nl,
+            (Senha \== 'banco', Senha \== 'cola', Senha \== 'dica' -> write('Senha errada, faca a materia de novo..'),nl,mainIncrementa,bd
+            ; Senha == 'banco' -> write('Aprovado!'),nl,zerar_contador
+            ; Senha == 'cola' -> write('O professor pegou voce colando... Reprovado!!!'),nl,mainIncrementa,bd
+            ; Senha == 'dica' -> write('Dica final: a+1, b+1, c+1,...'),nl,senha
+            ).
 
 mecflu:- nl,write('Enquanto você esta cursando a materia de Mecanica dos Fluidos, e chegou na prova final.'),nl,
          write('Você abre a prova e ve apenas uma questao, fica nervoso, MUITO nervoso porque nao sabe responder e decide chtar...'),nl,
@@ -327,4 +303,23 @@ mecflu:- nl,write('Enquanto você esta cursando a materia de Mecanica dos Fluido
          write('1 - sim'),nl,
          write('2 - nao'),nl,
          read(Escolha),nl,
-        (Escolha \== 1 -> write('Você chutou errado... Reprovado!'),mecflu,nl ; write('Você deu a sorte no chute! Aprovado!!')).
+        (Escolha \== 1 -> write('Você chutou errado... Reprovado!'),mainIncrementa,mecflu,nl ; write('Você deu a sorte no chute! Aprovado!!'),zerar_contador).
+
+eletricidade:-write('Bem-vindo(a) a eletricidade!'),nl,
+              write('Nessa matéria você terá que ligar todos as luzes da sala, em apenas 4 movimentos, você sabe que:'),nl,
+              write('As luzes da sala estão desligadas'),nl,
+              write('A sala possui 4 interruptores correspondendo as 5 luzes na sala, cada uma atua de uma maneira diferente;'),nl,
+              write('interruptor 1 - liga a luz 1'),nl,
+              write('interruptor 2 - liga as luzes 1, 2 e 5'),nl,
+              write('interruptor 3 - liga as luzes 3 e 4, e desliga a luz 4 caso ela esteja ligada'),nl,
+              write('interruptor 4 - liga a luz 4'),nl,
+              write('Suas respostas: '),nl,
+              nl,write('Escolha 1: '),read(Escolha1),nl,
+              (Escolha1 \== 4 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 1 correta!')),nl,
+              write('Escolha 2: '),read(Escolha2),nl,
+              (Escolha2 \== 2 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 2 correta!')),nl,
+              write('Escolha 3: '),read(Escolha3),nl,
+              (Escolha3 \== 3 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 3 correta!')),nl,
+              write('Escolha 4: '),read(Escolha4),nl,
+              (Escolha4 \== 4 -> write('Reprovado'),nl,mainIncrementa,eletricidade ; write('Escolha 4 correta!')),nl,
+              write('Parabéns você foi aprovado!'),nl,zerar_contador.
